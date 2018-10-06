a83fe8d8ee9ccbd8a54a5784dc154dd5fddcbab9
SummySumanth <sumanth261094@gmail.com>
2018-10-06 23:41:03 +0530

    promise 1

--- js_exercises/promises/promise1.js
+++ js_exercises/promises/promise1.js
@@ -1,1 +1,16 @@
-let promiseToCleanTheRoom = new Promise();
+let promiseToCleanTheRoom = new Promise(function(resolve, reject){
+    // Cleaning the room
+    let isClean = false;
+
+    if(isClean){
+        resolve('Clean');
+    }else{
+        reject('not clean');
+    }
+});
+
+promiseToCleanTheRoom.then(function(fromResolve){
+    console.log('the room is ' + fromResolve);
+}).catch(function(fromReject){
+    console.log('the room is ' + fromReject)
+});
