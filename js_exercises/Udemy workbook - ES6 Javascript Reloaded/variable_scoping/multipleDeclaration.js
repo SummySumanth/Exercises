1d28b9f7547adb6cc364afc616c848e10387841e
SummySumanth <sumanth261094@gmail.com>
2019-02-27 23:37:43 +0530

    moving files

---
+++ js_exercises/Udemy workbook - ES6 Javascript Reloaded/variable_scoping/multipleDeclaration.js
@@ -0,0 +1,10 @@
+var a = '123';
+var a = '345';
+console.log(a);
+
+let b = '123';
+let b = '345';
+console.log(b);
+
+// multiple declaration can be done in var where as in let , it will create a syntax error 
+// if same identifier is used in the same scope
