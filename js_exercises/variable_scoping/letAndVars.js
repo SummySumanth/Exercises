037794fa6c9ec67c7734279d1514b3cac9b0ecec
SummySumanth <sumanth261094@gmail.com>
2019-02-27 22:36:14 +0530

    Let and var scoping

---
+++ js_exercises/variable_scoping/letAndVars.js
@@ -0,0 +1,10 @@
+(()=>{
+    console.log(a); // Not defined 
+    let a = 2;
+    console.log(a); // 2
+    console.log(b); // undefined
+    var b = 3;
+    console.log(b); // 3
+})();
+
+// pay attention to not defined and undefined ... 
