8f76255cb594d28f5b33e709e07837fcfdd3a0b6
Unknown <sumanth261094@gmail.com>
2019-03-21 06:09:43 +0530

    sets

--- js_exercises/Udemy__ES6_Javascript_Reloaded/S6: Sets/sets.js
+++ js_exercises/Udemy__ES6_Javascript_Reloaded/S6: Sets/sets.js
@@ -16,6 +16,27 @@ cars2.add('Audi');
 
 console.log('total number of cars', cars2.size);
 
+// Sets 'for of' loop
 for(let car of cars2){
-    console.log(car);
+    console.log(car); // -> Audi BMW Mercedes
 }
+
+// Set Destructuring
+
+let [a,b,c] =  cars2;
+console.log(a,b,c);
+
+// weak sets
+let weakCars = new WeakSet();
+
+weakCars.add({driver : 'audi'}); //Weak sets accept only objects
+
+let passanger = {passanger:'sarah'};
+
+weakCars.add(passanger);
+
+console.log(weakCars.has(passanger)); // -> true
+
+weakCars.delete(passanger);
+
+console.log(weakCars.has(passanger)); // -> false
