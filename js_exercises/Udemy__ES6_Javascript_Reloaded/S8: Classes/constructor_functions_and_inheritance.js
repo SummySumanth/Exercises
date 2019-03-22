95810110f4c69ca68e974994de014e689bfb0e80
Unknown <sumanth261094@gmail.com>
2019-03-22 14:26:18 +0530

    constructor functions

---
+++ js_exercises/Udemy__ES6_Javascript_Reloaded/S8: Classes/constructor_functions_and_inheritance.js
@@ -0,0 +1,40 @@
+function Car(carSpec){
+    this.name = carSpec.name;
+    this.model = carSpec.model;
+    this.description = carSpec.description;
+}
+
+let car = new Car({ name: 'bmw', model: 'abc', description: 'hello world'});
+
+Car.prototype.drive = function(){
+    console.log('Driving...');
+}
+
+console.clear();
+
+console.log(car.description);
+
+// console.log(car.drive());
+
+car.drive();
+
+function Audi(carSpec){
+    Car.call(this, carSpec);
+    this.engine = carSpec.engine;
+    this.audiDrive = function(){
+        console.log('Audi Driving...');
+    }
+}
+
+Audi.prototype = Object.create(Car.prototype);
+
+Audi.prototype.constructor = Audi;
+
+const myAudi = new Audi({
+    name: 'My audi',
+    model: 'Audi',
+    description: 'abc 123',
+    engine: 'A313'
+});
+
+console.log(myAudi.description);
